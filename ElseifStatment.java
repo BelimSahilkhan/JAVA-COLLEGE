83ae958a56c3870293f70fef4eb29adb8328b033
S_K_Khan <159645700+BelimSahilkhan@users.noreply.github.com>
2024-11-26 05:12:36 -1100

    Add files via upload

---
+++ ElseifStatment.java
@@ -0,0 +1,21 @@
+class ElseifStatment
+{
+	public static void main(String args[])
+	{
+		int x=10;
+		if(x>20)
+		{
+			System.out.println("A is Greater than 20");
+		}
+		else if(x==10)
+		{
+			System.out.println("X is Equl To 10");
+		}
+		else
+		{	
+			System.out.println("X is Lessthan 10");
+		}
+	}
+}
+			
+	
